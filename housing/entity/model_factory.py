6ad4f21372306d82171e4eb7cf2da6f4577ac076
Linkan Sahu <sahulinkan7@gmail.com>
2023-01-26 21:44:35 +0530

    model_factory code updated

--- housing/entity/model_factory.py
+++ housing/entity/model_factory.py
@@ -5,7 +5,9 @@ import yaml
 from housing.constant import *
 from collections import namedtuple
 import numpy as np
-
+from sklearn.metrics import r2_score,mean_squared_error
+from typing import List
+import importlib
 InitializedModelDetail=namedtuple("InitializedModelDetail",
 ["model_serial_number","model","param_grid_search","model_name"])
 
@@ -41,7 +43,57 @@ def evaluate_regression_model(model_list:list,x_train:np.ndarray,y_train:np.ndar
     
     """
     try:
-        pass
+        index_number=0
+        metric_info_artifact=None
+        for model in model_list:
+            model_name=str(model)
+            logging.info(f"{'>>'*30} started evaluating model [{type(model).__name__}]{'<<'*30}")
+
+
+            #getting prediction for training and testing dataset
+            y_train_pred=model.predict(x_train)
+            y_test_pred=model.predict(x_test)
+
+            #calculating r squared score on training and testing dataset
+            train_acc=r2_score(y_train,y_train_pred)
+            test_acc=r2_score(y_test,y_test_pred)
+
+            #calculating mean squared error on training and testing dataset
+            train_rmse=np.sqrt(mean_squared_error(y_train,y_train_pred))
+            test_rmse=np.sqrt(mean_squared_error(y_test,y_test_pred))
+
+            #calculating harmonic mean of train accuracy and test accuracy
+            model_accuracy=(2*(train_acc*test_acc))/ (train_acc+test_acc)
+            diff_test_train_acc=abs(test_acc-train_acc)
+
+            #logging important metrics
+            logging.info(f"{'>>'*30} Score {'<<'*30}")
+            logging.info(f"Train Score\t\t Test Score\t\t Average Score")
+            logging.info(f"{train_acc}\t\t{test_acc}\t\t{model_accuracy}")
+
+            logging.info(f"{'>>'*30} Loss {'<<'*30}")
+            logging.info(f"Diff test train accuracy: [{diff_test_train_acc}]")
+            logging.info(f"Train root mean squared error: [{train_rmse}]")
+            logging.info(f"Test root mean squared error: [{test_rmse}]")
+
+            #if model accuracy is greater than the base accuracy and train and test score are within certain threshold
+            #we will accept the model
+
+            if model_accuracy>=base_accuracy and diff_test_train_acc<0.5:
+                base_accuracy=model_accuracy
+                metric_info_artifact=MetricInfoArtifact(model_name=model_name,
+                                                        model_object=model,
+                                                        train_rmse=train_rmse,
+                                                        test_rmse=test_rmse,
+                                                        train_accuracy=train_acc,
+                                                        test_accuracy=test_acc,
+                                                        model_accuracy=model_accuracy,
+                                                        index_number=index_number)
+                logging.info(f"Acceptable model found {metric_info_artifact}")
+            index_number+=1
+        if metric_info_artifact is None:
+            logging.info(f"No model found with better accuracy than base accuracy")
+        return metric_info_artifact
     except Exception as e:
         raise HousingException(e,sys) from e
 
@@ -54,11 +106,56 @@ class ModelFactory:
             self.grid_search_cv_module: str=self.config[GRID_SEARCH_KEY][MODULE_KEY]
             self.grid_search_class_name: str=self.config[GRID_SEARCH_KEY][CLASS_KEY]
             self.grid_search_property_data: dict=dict(self.config[GRID_SEARCH_KEY][PARAM_KEY])
-            self.model_initialization_config: dict=dict(self.config[MODEL_SELECTION_KEY])
+            self.models_initialization_config: dict=dict(self.config[MODEL_SELECTION_KEY])
 
             self.initialized_model_list=None
             self.grid_searched_best_model_list=None
 
+        except Exception as e:
+            raise HousingException(e,sys) from e
+
+    def get_initialized_model_list(self)->List[InitializedModelDetail]:
+        """
+        This function will return a list of model details
+        return list[ModelDetail]
+        """
+        try:
+            initialized_model_list=[]
+            for model_serial_number in self.models_initialization_config.keys():
+
+                model_initialization_config=self.models_initialization_config[model_serial_number]
+                model_obj_ref=ModelFactory.class_for_name(module_name=model_initialization_config[MODULE_KEY],
+                                                            class_name=model_initialization_config[CLASS_KEY])
+
+                model=model_obj_ref()
+
+                if PARAM_KEY in model_initialization_config:
+                    model_obj_property_data=dict(model_initialization_config[PARAM_KEY])
+                    model=ModelFactory.update_property_of_class(instance_ref=model,property_data=model_obj_property_data)
+                
+                param_grid_search=model_initialization_config[SEARCH_PARAM_GRID_KEY]
+                model_name=f"{model_initialization_config[MODULE_KEY]}.{model_initialization_config[CLASS_KEY]}"
+
+                model_initialization_config=InitializedModelDetail(model_serial_number=model_serial_number,
+                                                                    model=model,
+                                                                    param_grid_search=param_grid_search,
+                                                                    model_name=model_name)
+                initialized_model_list.append(model_initialization_config)
+
+            self.initialized_model_list=initialized_model_list
+            return self.initialized_model_list
+        except Exception as e:
+            raise HousingException(e,sys) from e
+    @staticmethod
+    def update_property_of_class(instance_ref:object,property_data:dict):
+        try:
+            if not isinstance(property_data,dict):
+                raise Exception("property_data parameter is required in dictionary")
+            print(property_data)
+            for key,value in property_data.items():
+                logging.info(f"Executing:$ {str(instance_ref)}.{key}={value}")
+                setattr(instance_ref,key,value)
+            return instance_ref
         except Exception as e:
             raise HousingException(e,sys) from e
 
@@ -71,8 +168,67 @@ class ModelFactory:
         except Exception as e:
             raise HousingException(e,sys) from e
 
+    @staticmethod
+    def class_for_name(module_name:str,class_name:str):
+        try:
+            #load the module wil raise error if module can not be loaded
+            module=importlib.import_module(module_name)
+            #get the class, will raise Attribute error if class can not be found
+            logging.info(f"Executing command: from {module} import {class_name}")
+            class_ref=getattr(module,class_name)
+            return class_ref
+        except Exception as e:
+            raise HousingException(e,sys) from e
+
+    def initiate_best_parameter_search_for_initialized_model(self,initialized_model: InitializedModelDetail,
+                                                                input_feature,
+                                                                output_feature
+                                                                ) -> GridSearchedBestModel:
+        """
+        initiate_best_model_parameter_search(): function will perform paramter search operation and
+        it will return you the best optimistic  model with best paramter:
+        estimator: Model object
+        param_grid: dictionary of paramter to perform search operation
+        input_feature: your all input features
+        output_feature: Target/Dependent features
+        ================================================================================
+        return: Function will return a GridSearchOperation
+        """
+        try:
+            return self.execute_grid_search_operation(initialized_model=initialized_model,
+                                                        input_feature=input_feature,
+                                                        output_feature=output_feature)
+        except Exception as e:
+            raise HousingException(e,sys) from e
+    
+    def inititate_best_parameter_search_for_initialized_models(self,
+                                                                initialized_model_list:List[InitializedModelDetail],
+                                                                input_feature,
+                                                                output_feature) -> List[GridSearchedBestModel]:
+        try:
+            self.grid_searched_best_model_list=[]
+            for initialized_model_list in initialized_model_list:
+                grid_searched_best_model=self.initiate_best_parameter_search_for_initialized_model(
+                    initialized_model=initialized_model_list,
+                    input_feature=input_feature,
+                    output_feature=output_feature
+                )
+                self.grid_searched_best_model_list.append(grid_searched_best_model)
+            return self.grid_searched_best_model_list
+        except Exception as e:
+            raise HousingException(e,sys) from e
+
     def get_best_model(self,x,y,base_accuracy=0.6) -> BestModel:
         try:
-            pass
+            logging.info(f"Started Initializing model from config file")
+            initialized_model_list=self.get_initialized_model_list()
+            logging.info(f"Initialized model: {initialized_model_list}")
+
+            grid_searched_best_model_list=self.initiate_best_parameter_search_for_initialized_models(
+                initialized_model_list=initialized_model_list,
+                input_feature=x,
+                output_feature=y
+            )
+
         except Exception as e:
             raise HousingException(e,sys) from e
